9fecfa5775b5f8ff737bb80dba12999f707c33db
Raki/Shubh <edits000.11@gmail.com>
2025-05-23 03:39:09 +0530

    selenium with java beginning
    just some beginner level files

---
+++ Selenium-with-Java/selenium-with-java/src/main/java/com/rakesh/selenium/selenium_with_java/Firstselenium_class.java
@@ -0,0 +1,11 @@
+package com.rakesh.selenium.selenium_with_java;
+
+public class Firstselenium_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
