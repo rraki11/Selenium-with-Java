02f68116d20982ac26b3a2cccd54e8015633497f
Raki/Shubh <edits000.11@gmail.com>
2025-05-24 04:42:04 +0530

    a program to demonstrate the use of WebDriverManager
    Uploaded the program to demonstrate the use of WebDrivers.
    
    Created a new java class files "WebDrvier_manager.java".
    
    Added the dependency for WebDriverManger in the pom.xml file.
    
    -> Remember to add the WebDriverManager dependency in the pom.xml file to use the WebDRriverManager.
    
    -> WebDriver automatically downloads the drivers of the specific version of the browser you have. So, we don't need to download them manually again and again if our browsers version changes.

---
+++ selenium-with-java/src/main/java/com/rakesh/selenium/selenium_with_java/WebDriver_manager.java
@@ -0,0 +1,34 @@
+package com.rakesh.selenium.selenium_with_java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriver_manager {
+
+	public static void main(String[] args) {
+
+		// Before using WebDriverManager, make sure its dependency is added in your pom.xml (for Maven)
+		// WebDriverManager helps automatically download the correct browser driver version for you.
+		// This means you don’t have to manually download and set the path to the browser driver.
+
+		// Automatically sets up the ChromeDriver based on the installed version of Chrome browser.
+		WebDriverManager.chromedriver().setup(); 
+
+		/*
+		 * If you're working in a network environment with proxy restrictions (like in some companies),
+		 * you can configure the proxy as shown below:
+		 * WebDriverManager.chromedriver().proxy("your-proxy-host:port").setup();
+		 */
+
+		// Launch a new Chrome browser instance
+		WebDriver driver = new ChromeDriver();
+
+		// Open Google's homepage
+		driver.get("https://google.com/");
+
+		// Close the browser
+		driver.quit();
+	}
+}
