b2035a2bdf53ba83a120ead5b1e13687b234c174
Raki/Shubh <edits000.11@gmail.com>
2025-06-26 18:20:02 +0530

    Revert "Create smth.java"
    This reverts commit b7d813d3e947b346fb61697d33ef791e275e85b5.

---
+++ selenium-with-java/src/main/java/com/rakesh/selenium/selenium_with_java/_24_Calender_practice.java
@@ -0,0 +1,32 @@
+package com.rakesh.selenium.selenium_with_java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class _24_Calender_practice {
+
+	public static void main(String[] args)throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.hyrtutorials.com/p/calendar-practice.html");
+		
+		Actions action = new Actions(driver);
+		//calender one
+		Thread.sleep(3000);
+		
+		driver.findElement(By.id("second_date_picker")).click();
+		Thread.sleep(2000);
+		action.doubleClick(driver.findElement(By.xpath("//a[@class='ui-datepicker-next ui-corner-all']"))).perform();
+
+		
+		
+
+	}
+
+}
