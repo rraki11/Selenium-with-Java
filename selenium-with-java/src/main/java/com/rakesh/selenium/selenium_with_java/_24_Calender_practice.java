df5b2c8fd57a9fad2606e3838e7bdaf1c5cccffd
Raki/Shubh <edits000.11@gmail.com>
2025-08-24 06:20:01 +0530

    calender and java script executor edited and created

--- selenium-with-java/src/main/java/com/rakesh/selenium/selenium_with_java/_24_Calender_practice.java
+++ selenium-with-java/src/main/java/com/rakesh/selenium/selenium_with_java/_24_Calender_practice.java
@@ -2,9 +2,8 @@ package com.rakesh.selenium.selenium_with_java;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class _24_Calender_practice {
@@ -16,25 +15,25 @@ public class _24_Calender_practice {
 		driver.manage().window().maximize();
 		driver.get("https://www.hyrtutorials.com/p/calendar-practice.html");
 		
-		Actions action = new Actions(driver);
-		int day = 23;
+		int date1 = 23;
+		int date2 = 30;
 		
-		//calender two
-		Thread.sleep(3000);
+		// calender type 1
+		WebElement picker1 = driver.findElement(By.id("first_date_picker"));
+		picker1.click();
+		driver.findElement(By.xpath("//table[@class='ui-datepicker-calendar']//a[text()="+date1+"]")).click();
+//		String pickervalue1 = picker1.getAttribute("value");
+		String pickervalue1 = picker1.getDomProperty("value");
+		System.out.println("calender 1: " + pickervalue1);
 		
-		driver.findElement(By.id("second_date_picker")).click();
-		Thread.sleep(2000);
-		action.doubleClick(driver.findElement(By.xpath("//a[@class='ui-datepicker-next ui-corner-all']"))).perform();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//table[contains(@class,'ui-datepicker-calendar')]//tr//td[not(contains(@class,' ui-datepicker-other-month'))]//a[text()="+day+"]")).click();
-
-		//calender three
-		Thread.sleep(3000);
+		// calender type 2
+		WebElement picker2 =  driver.findElement(By.id("second_date_picker"));
+		picker2.click();
+		driver.findElement(By.xpath("//table[@class='ui-datepicker-calendar']//td[not(contains(@class,' ui-datepicker-other-month'))]/a[text()="+date2+"]")).click();
+		String pickervalue2 = picker2.getDomProperty("value");
+		System.out.println("calender 2: " + pickervalue2);
 		
-		driver.findElement(By.id("third_date_picker")).click();
-		Thread.sleep(2000);
 		
-
 	}
 
 }
--- selenium-with-java/src/main/java/com/rakesh/selenium/selenium_with_java/_27_JavascriptExecutor.java
+++ selenium-with-java/src/main/java/com/rakesh/selenium/selenium_with_java/_27_JavascriptExecutor.java
@@ -1,8 +1,11 @@
 package com.rakesh.selenium.selenium_with_java;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class _27_JavascriptExecutor {
@@ -19,25 +22,60 @@ public class _27_JavascriptExecutor {
 		
 		// finding element by ID and passing a Value using java script executor
 		jsExecutor.executeScript("document.getElementById(\"email\").value='found by ID';");
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		// finding element by Name and passing a value
 		jsExecutor.executeScript("document.getElementsByName(\"email\")[0].value='found by Name';");
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		//finding element by TagName and passing a value
 		jsExecutor.executeScript("document.getElementsByTagName(\"input\")[2].value='found by Tagname';"); 
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		//finding element by ClassName and passing a value 
 		jsExecutor.executeScript("document.getElementsByClassName(\"inputtext\")[0].value='found by Classname';");
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		//finding element by CSS selector and passing a value
 		jsExecutor.executeScript("document.querySelector(\"#email\").value='found by CssSelector';");
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		//finding element by xpath and passing a value
 		jsExecutor.executeScript("document.evaluate(\"//input[@id='email']\",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue.value='found by xpath';");
+		Thread.sleep(2000);
 		
 		// finding and clicking 
 		jsExecutor.executeScript("document.getElementsByName(\"login\")[0].click();");
 		
+		// highlighting using javascript executor 
+		Thread.sleep(2000);
+		jsExecutor.executeScript("document.getElementsByName(\"login\")[0].style.border='3px black solid';"); //highlighting using border style attribute
 		
+		Thread.sleep(2000);
+		jsExecutor.executeScript("document.getElementsByName(\"email\")[0].style.background='lightblue';"); //highlighting using background style attribute
+		
+		Thread.sleep(2000);
+		jsExecutor.executeScript("document.getElementsByName(\"pass\")[0].setAttribute('style','border:3px black solid;background:lightblue');"); //hgihlighting using both styles by setAttribute
+		
+		// Scrolling by using JavaScript Executor
+		driver.get("https://www.hyrtutorials.com/p/calendar-practice.html");
+		
+		Thread.sleep(2000);
+		// window.scrollTo(x,y)
+		jsExecutor.executeScript("window.scrollTo(0,500);");
+		
+		Thread.sleep(2000);
+		// window.scrollBy(x,y)
+		jsExecutor.executeScript("window.scrollBy(0,500);");
+		
+		Thread.sleep(2000);
+		// window.scrollIntoView()
+		jsExecutor.executeScript("document.getElementById('first_date_picker').scrollIntoView();");
+		
+		Thread.sleep(2000);
+		// Highlighting by using javaScriptExecutor arguments and webelement object
+		driver.navigate().to("https://www.facebook.com/");
+		Thread.sleep(2000);
+		
+		WebElement emailElement = driver.findElement(By.id("email"));
+		jsExecutor.executeScript("arguments[0].setAttribute('style','border:3px black solid;background:lightblue');", emailElement);
+		Thread.sleep(2000);
+		
+		driver.close();
     }
 }
